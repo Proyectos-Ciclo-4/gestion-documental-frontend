98197c62296f8335a81c3e48c3614f1b67fca198
Leonardo henao <leonardofhenao.085@gmail.com>
2022-09-23 11:55:57 -0500

    Login finalizado
    Variables de entorno configuradas

--- src/app/services/endpoints/endpoints.service.ts
+++ src/app/services/endpoints/endpoints.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Host, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { ResponseVerify } from 'src/app/utils/models/responseVerify';
+import { environment } from 'src/environments/environment.prod';
 
 
 @Injectable({
@@ -9,13 +10,14 @@ import { ResponseVerify } from 'src/app/utils/models/responseVerify';
 })
 export class EndpointsService {
 
-  urlHost = 'http://localhost:8080';
-
   constructor(private http: HttpClient) { }
 
-  // respuesta { response: ok, id-user: 12345, rol: 700}
-  verifyUser(body: any): Observable<ResponseVerify> {
-    return this.http.post<ResponseVerify>(this.urlHost, { ...body });
+  verifyUser(email: string): Observable<ResponseVerify> {
+    return this.http.get<ResponseVerify>(`${environment.host.verifyUser}${email}`);
+  }
+
+  createDocument(document: object): Observable<object> {
+    return this.http.post(environment.host.createDocument, { ...document });
   }
 
 }
