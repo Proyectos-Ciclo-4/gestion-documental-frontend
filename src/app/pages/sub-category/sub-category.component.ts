1f5a9f730b4722f405e15396e16943ce7d0ee2d8
ACamilo <andresdm1997@gmail.com>
2022-09-25 12:55:58 -0500

    Se refactorizó la obtención de datos desde el crear subcategory, formulario 100% funcional

--- src/app/pages/sub-category/sub-category.component.ts
+++ src/app/pages/sub-category/sub-category.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Category } from 'src/app/models/category.model';
 import { SubCategory } from 'src/app/models/subcategory.model';
 import { EndpointsService } from 'src/app/services/endpoints/endpoints.service';
@@ -7,10 +7,9 @@ import { EndpointsService } from 'src/app/services/endpoints/endpoints.service';
 @Component({
   selector: 'sub-category',
   templateUrl: './sub-category.component.html',
-  styleUrls: ['./sub-category.component.css']
+  styleUrls: ['./sub-category.component.css'],
 })
 export class SubCategoryComponent implements OnInit {
-
   nombre: string = '';
   body: NgForm;
   formState: Boolean = false;
@@ -21,42 +20,58 @@ export class SubCategoryComponent implements OnInit {
   categoryName: string = '';
   selectedOption: string;
 
+  documentForm = new FormGroup({
+    name: new FormControl('', { validators: [Validators.required] }),
+    category: new FormControl('', { validators: [Validators.required] }),
+  });
 
-  constructor(private service: EndpointsService) { }
+  constructor(private service: EndpointsService) {}
+
+  protected async submit() {
+
+    const docName = this.documentForm.get('name');
+    const docCategory = this.documentForm.get('category');
+    console.log("name:", docName.value)
+    console.log("category:", docCategory.value)
+    this.service.createSubCategory({
+      categoryId: docCategory.value,
+      subCategoryName: docName.value,
+    }).subscribe(n => {
+      docName.setValue("");
+      docCategory.setValue("");
+      this.showModalCatgoryCreated = true;
+    });
+  }
 
   ngOnInit(): void {
-    this.getCategoryList()
-
+    this.getCategoryList();
   }
-
-  createSubCategory(body: NgForm){
-    console.log("ds",body)
-    console.log("dentro de id:",this.subcategory.categoryId)
-    console.log("dentro de crear subcategoria:",this.subcategory.subCategoryName)
-    this.service.createSubCategory(this.subcategory).subscribe({
+  getSubcategoriesByCategory() {
+    const docCategory = this.documentForm.get('category');
+    this.service.getSubCategories(docCategory.value).subscribe({
       next: (res) => {
-        this.showModalCatgoryCreated = true;
-      }
-    })
+        this.subcategories = res;
+      },
+    });
   }
-  revealForm(){
-    this.formState = !this.formState
+  revealForm() {
+    this.formState = !this.formState;
   }
   getSubCategoryList() {
     this.service.getSubCategories(this.subcategory.categoryId).subscribe({
       next: (res) => {
         this.subcategories = res;
-      }
-    })
+      },
+    });
   }
-  selectCategory(){
-    this.getSubCategoryList()
+  selectCategory() {
+    this.getSubCategoryList();
   }
   getCategoryList() {
     this.service.getAllCategories().subscribe({
       next: (res) => {
         this.categories = res;
-      }
-    })
+      },
+    });
   }
 }
